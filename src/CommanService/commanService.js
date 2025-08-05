8c0ed91617f378380e7db138224a478b3d2386a7
bhaveshambaliya999 <120005394+bhaveshambaliya999@users.noreply.github.com>
2025-08-05 10:07:48 +0530

    new

--- src/CommanService/commanService.js
+++ src/CommanService/commanService.js
@@ -5,23 +5,23 @@ function getEnv(origin) {
   const isBrowser = typeof window !== "undefined";
   let env = {};
 
-  const source = origin || (isBrowser ? window.location.origin : "");
+  const source = origin || (isBrowser ? window.location.origin : origin);
 
-  if (source.includes("localhost")) {
+  if (source?.includes("localhost")) {
     env = {
       imageUrl: "http://192.168.84.21:8080",
       baseUrl1: "http://192.168.84.21:8080/api/call",
       baseUrl2: "http://192.168.84.45/sit-ci-api/call",
       domain: "https://zurah-next.vercel.app",
     };
-  } else if (source.includes("staging")) {
+  } else if (source?.includes("staging")) {
     env = {
       imageUrl: "https://apistaging-ecom-store.upqor.com",
       baseUrl1: "https://apistaging-ecom-store.upqor.com/api/call",
       baseUrl2: "https://apistaging-ecom.upqor.com/call",
       domain: source,
     };
-  } else if (source.includes("uat")) {
+  } else if (source?.includes("uat")) {
     env = {
       imageUrl: "https://apiuat-ecom-store.upqor.com",
       baseUrl1: "https://apiuat-ecom-store.upqor.com/api/call",
