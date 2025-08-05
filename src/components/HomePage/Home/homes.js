a2061a053b4f4bed5b9aa263801dac208f78e49c
bhaveshambaliya999 <120005394+bhaveshambaliya999@users.noreply.github.com>
2025-08-05 09:36:37 +0530

    new

--- src/components/HomePage/Home/homes.js
+++ src/components/HomePage/Home/homes.js
@@ -73,9 +73,7 @@ const Homes = (props) => {
         keywords: storeEntityIds?.seo_keyword,
         image: HeaderLogoData?.[0]?.image,
         url: typeof window !== "undefined" && window.location.href,
-        
     }
-      console.log(data?.seo_titles);
 
     //Function for most searchable product by API calling 
     const mostSearchableData = useCallback((sectionDataList, mainData, index) => {
