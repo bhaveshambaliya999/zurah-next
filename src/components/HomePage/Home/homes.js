e458449a480d2ae2498031ada19421f9e6bfeaa4
bhaveshambaliya999 <120005394+bhaveshambaliya999@users.noreply.github.com>
2025-08-05 09:56:42 +0530

    new

--- src/components/HomePage/Home/homes.js
+++ src/components/HomePage/Home/homes.js
@@ -68,18 +68,12 @@ const Homes = (props) => {
     var storeData = JSON.parse(typeof window !== "undefined" && sessionStorage.getItem("storeData"))
 
     const metaConfig = {
-  title: storeEntityIds?.seo_titles || "Zurah Jewellery",
-  description: storeEntityIds?.seo_description || "Default description",
-  keywords: storeEntityIds?.seo_keywords || "zurah, jewellery",
-  image:
-    HeaderLogoData?.[0]?.image && HeaderLogoData[0].image.startsWith("http")
-      ? HeaderLogoData[0].image
-      : "https://yourdomain.com/default.jpg",
-  url:
-    typeof window !== "undefined"
-      ? window.location.href
-      : "https://zurah-next.vercel.app/",
-};
+        title: storeEntityIds?.seo_titles,
+        description: storeEntityIds?.seo_description,
+        keywords: storeEntityIds?.seo_keyword,
+        image: HeaderLogoData?.[0]?.image,
+        url: typeof window !== "undefined" && window.location.href,
+    }
     console.log("✅ Meta Config:", metaConfig);
 
     //Function for most searchable product by API calling 
