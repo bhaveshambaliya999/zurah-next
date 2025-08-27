90a11d62c0a1b7887776c7b9b3776bf7f7d35ae0
bhaveshambaliya999 <120005394+bhaveshambaliya999@users.noreply.github.com>
2025-08-27 12:33:38 +0530

    new

--- src/CommanUIComp/Loader/Loader.jsx
+++ src/CommanUIComp/Loader/Loader.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import styles from "./loader.module.scss";
 import { Oval } from 'react-loader-spinner';
 
 const Loader = () => {
     return (
         <>
             <section id="loader">
-                <div className={styles["loader"]}>
+                <div className="loader">
                     <Oval
                         height={100}
                         width={100}
